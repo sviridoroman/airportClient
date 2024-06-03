7f8c716da7956360f2352259abfa9c676bee1ec8
sviridoroman <sviridoroman@gmail.com>
2024-06-03 03:08:13 +0300

    redux store added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './store/index.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>       
       <App /><link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&display=swap" rel="stylesheet"/>
+    </Provider>
   </React.StrictMode>,
 )
